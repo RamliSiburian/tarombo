e33408a5448da4c004be0ee1385b6f3613f658b7
RamliSiburian <ramlisiburian1997@gmail.con>
2026-09-02 00:09:02 +0700

    mengubah redirect ke dashboardketika berhasil login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -33,7 +33,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('tree.index', absolute: false));
+        return redirect()->intended(route('admin.dashboard', absolute: false));
     }
 
     /**
